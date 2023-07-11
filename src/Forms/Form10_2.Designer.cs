403c9fbfc7511ba6f75d2d8177d6f615585e8441
Leo <leo.guo@aisushi.com.cn>
2023-07-11 17:41:06 +0800

    feat: 10.2 颜色空间转换与物体追踪

--- src/Forms/Form10_2.Designer.cs
+++ src/Forms/Form10_2.Designer.cs
@@ -29,23 +29,34 @@
         private void InitializeComponent()
         {
             pcbImageShow = new PictureBox();
+            btnRun = new Button();
             ((System.ComponentModel.ISupportInitialize)pcbImageShow).BeginInit();
             SuspendLayout();
             // 
             // pcbImageShow
             // 
-            pcbImageShow.Dock = DockStyle.Fill;
-            pcbImageShow.Location = new Point(0, 0);
+            pcbImageShow.Location = new Point(0, 45);
             pcbImageShow.Name = "pcbImageShow";
-            pcbImageShow.Size = new Size(800, 450);
+            pcbImageShow.Size = new Size(800, 405);
             pcbImageShow.TabIndex = 0;
             pcbImageShow.TabStop = false;
+            // 
+            // btnRun
+            // 
+            btnRun.Location = new Point(0, 3);
+            btnRun.Name = "btnRun";
+            btnRun.Size = new Size(800, 38);
+            btnRun.TabIndex = 1;
+            btnRun.Text = "打开摄像头，进行颜色空间转换";
+            btnRun.UseVisualStyleBackColor = true;
+            btnRun.Click += btnRun_Click;
             // 
             // Form10_2
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnRun);
             Controls.Add(pcbImageShow);
             Name = "Form10_2";
             Text = "10.2 颜色空间转换与物体追踪";
@@ -57,5 +68,6 @@
         #endregion
 
         private PictureBox pcbImageShow;
+        private Button btnRun;
     }
 }
--- src/Forms/Form10_2.cs
+++ src/Forms/Form10_2.cs
@@ -1,4 +1,5 @@
 ﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -23,23 +24,64 @@ namespace NC.OpenCvSharp4.Learning.Forms
 
         }
 
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Run();
+        }
+
         private void Run()
         {
-            VideoCapture Cap = new VideoCapture();
+            VideoCapture capture = new VideoCapture();
             // 打开ID为0的摄像头
-            Cap.Open(0);
+            capture.Open(0);
             // 判断摄像头是否成功打开
-            if (!Cap.IsOpened())
+            if (!capture.IsOpened())
             {
                 MessageBox.Show("摄像头打开失败.");
                 return;
             }
 
-            var frame = new Mat();
+            capture.FrameWidth = pcbImageShow.Width;
+            capture.FrameHeight = pcbImageShow.Height;
+            // RGB
+            // BGR
+            // 设置颜色阈值，BGR存储顺序，非常规的RGB
+            var lower = new Scalar(19, 78, 39); // RGB(39,78,19) // new Scalar(90, 50, 50);
+            var upper = new Scalar(168, 255, 182);  // RGB(182,215,168) // new Scalar(130, 255, 255);
 
-            // 设置颜色阈值，BGR存储顺序，非常规RGB
-            var lower = new Scalar(90, 50, 50);
-            var upper = new Scalar(130, 255, 255);
+            var frame = new Mat();
+            while (true)
+            {
+                if (capture.Read(frame))
+                {
+                    var hsv = new Mat();
+                    var res = new Mat();
+                    var mask = new Mat();
+
+                    // 切换到 HSV
+                    Cv2.CvtColor(frame, hsv, ColorConversionCodes.BGR2HSV);
+
+                    // 根据阈值构建掩模
+                    // 检查数组元素是否位于其他两个数组的元素之间
+                    Cv2.InRange(hsv, lower, upper, mask);
+
+                    // 对原图像和掩模进行位运算
+                    Cv2.BitwiseAnd(frame, frame, res, mask);
+
+                    // 显示图像
+                    pcbImageShow.Image = BitmapConverter.ToBitmap(frame); // 原图
+
+                    Cv2.ImShow("mask", mask);
+                    //Cv2.MoveWindow("mask", frame.Width, 0);// 右边
+                    Cv2.ImShow("res", res);
+                    //Cv2.MoveWindow("res", 0, frame.Height);// 下边
+
+                    if (Cv2.WaitKey(10) == 27)
+                    {
+                        break;
+                    }
+                }
+            }
         }
     }
 }
